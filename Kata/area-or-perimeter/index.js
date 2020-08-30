e109ed258c23904b279d95cf53d514e508f0808a
R00GER <roni.friman@sonit.fi>
2020-08-30 21:18:56 +0300

    Add new Kata

---
+++ Kata/area-or-perimeter/index.js
@@ -0,0 +1,15 @@
+// https://www.codewars.com/kata/5ab6538b379d20ad880000ab
+
+// You are given the length and width of a 4-sided polygon. The polygon can either be a rectangle or a square.
+// If it is a square, return its area. If it is a rectangle, return its perimeter.
+
+// area_or_perimeter(6, 10) --> 32
+// area_or_perimeter(4, 4) --> 16
+
+const areaOrPerimeter = (l, w) => {
+  if (l === w) return l * w;
+  else return l * 2 + w * 2;
+};
+
+console.log(areaOrPerimeter(4, 4));
+console.log(areaOrPerimeter(6, 10));
